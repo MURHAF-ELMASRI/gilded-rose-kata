894ff81734a81ed37a6e8ea96f3cf25a30287254
murhaf Elmasri <murhaf.msri@gmail.com>
2022-11-03 16:39:36 +0300

    separate test units into files

---
+++ test/mocha/commonRules.spec.ts
@@ -0,0 +1,25 @@
+import { GildedRose } from "@/gildedRose";
+import { Item } from "@/Item";
+import { expect } from "chai";
+
+describe("common rules for all product", () => {
+  it("should foo", () => {
+    const gildedRose = new GildedRose([new Item("foo", 0, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).to.equal("foo");
+  });
+
+  it("should items be for empty collection", () => {
+    const gildedRose = new GildedRose();
+    const items = gildedRose.updateQuality();
+    expect(items).to.deep.equal([]);
+  });
+
+  it("item has SellIn, Quality and name properties", () => {
+    const gildedRose = new GildedRose([new Item("foo", 0, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0]).to.have.property("name");
+    expect(items[0]).to.have.property("quality");
+    expect(items[0]).to.have.property("sellIn");
+  });
+});
--- test/mocha/normalItem.spec.ts
+++ test/mocha/normalItem.spec.ts
@@ -2,32 +2,6 @@ import { GildedRose } from "@/gildedRose";
 import { Item } from "@/Item";
 import { expect } from "chai";
 
-describe("common rules for all product", () => {
-  it("should foo", () => {
-    const gildedRose = new GildedRose([new Item("foo", 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).to.equal("foo");
-  });
-
-  it("should items be for empty collection", () => {
-    const gildedRose = new GildedRose();
-    const items = gildedRose.updateQuality();
-    expect(items).to.deep.equal([]);
-  });
-
-  it("item has SellIn, Quality and name properties", () => {
-    const gildedRose = new GildedRose([new Item("foo", 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0]).to.have.property("name");
-    expect(items[0]).to.have.property("quality");
-    expect(items[0]).to.have.property("sellIn");
-  });
-});
-/**
- * new Item("+5 Dexterity Vest", 10, 20),
-  new Item("Elixir of the Mongoose", 5, 7),
- */
-
 describe("Normal Items", () => {
   it("quality should decrease 2 times faster after sellIn pass", () => {
     const gildedRose = new GildedRose([new Item("+5 Dexterity Vest", 10, 20)]);
