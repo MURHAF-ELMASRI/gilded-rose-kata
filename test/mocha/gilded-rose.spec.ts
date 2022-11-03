c946b5c55c6ffb57b2825ad7277f59011c0ce077
murhaf Elmasri <murhaf.msri@gmail.com>
2022-11-03 15:26:23 +0300

    create general test cases

--- test/mocha/gilded-rose.spec.ts
+++ test/mocha/gilded-rose.spec.ts
@@ -1,10 +1,24 @@
-import { expect } from 'chai';
-import { Item, GildedRose } from '@/gilded-rose';
+import { GildedRose, Item } from "@/gilded-rose";
+import { expect } from "chai";
 
-describe('Gilded Rose', () => {
-  it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
+describe("common rules for all product", () => {
+  it("should foo", () => {
+    const gildedRose = new GildedRose([new Item("foo", 0, 0)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).to.equal('fixme');
+    expect(items[0].name).to.equal("foo");
+  });
+
+  it("should items be for empty collection", () => {
+    const gildedRose = new GildedRose();
+    const items = gildedRose.updateQuality();
+    expect(items).to.deep.equal([]);
+  });
+
+  it("item has SellIn, Quality and name properties", () => {
+    const gildedRose = new GildedRose([new Item("foo", 0, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0]).to.have.property("name");
+    expect(items[0]).to.have.property("quality");
+    expect(items[0]).to.have.property("sellIn");
   });
 });
