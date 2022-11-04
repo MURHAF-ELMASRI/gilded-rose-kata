94c2bd44f1f060b95ae0ab44c58b61dc74e4a272
murhaf Elmasri <murhaf.msri@gmail.com>
2022-11-04 10:35:08 +0300

    cover test case for normal item when quality be negative

--- test/mocha/normalItem.spec.ts
+++ test/mocha/normalItem.spec.ts
@@ -27,4 +27,14 @@ describe("Normal Items", () => {
       () => new GildedRose([new Item("+5 Dexterity Vest", 10, -20)])
     ).to.throw(errors.qualityError);
   });
+
+  it("quality should not be negative", () => {
+    const day = 10;
+    const gildedRose = new GildedRose([new Item("+5 Dexterity Vest", 10, 8)]);
+    for (let i = 0; i < day; i++) {
+      gildedRose.updateQuality();
+    }
+    expect(gildedRose.items[0].quality).to.equal(0);
+    expect(gildedRose.items[0].sellIn).to.equal(0);
+  });
 });
