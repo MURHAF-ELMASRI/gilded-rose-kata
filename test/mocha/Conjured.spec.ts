d91b236ed808a46cd0f1edef3b84392d81f11991
murhaf Elmasri <murhaf.msri@gmail.com>
2022-11-04 10:30:30 +0300

    cover test case after sellIn pass for conjured item

--- test/mocha/Conjured.spec.ts
+++ test/mocha/Conjured.spec.ts
@@ -4,7 +4,7 @@ import { spacialItemName } from "@/spacialItemName";
 import { expect } from "chai";
 
 describe("Conjured Items", () => {
-  it("should quality degrades 2 time faster", () => {
+  it("should quality degrades 2 time faster before sellIn passes", () => {
     const day = 5;
     const gildedRose = new GildedRose([
       new Item(spacialItemName.conjured, 5, 10),
@@ -14,5 +14,29 @@ describe("Conjured Items", () => {
     }
     expect(gildedRose.items[0].quality).to.equal(0);
     expect(gildedRose.items[0].sellIn).to.equal(0);
+  });
+
+  it("should quality degrades 4 time faster after sellIn passes", () => {
+    const day = 3;
+    const gildedRose = new GildedRose([
+      new Item(spacialItemName.conjured, 2, 10),
+    ]);
+    for (let i = 0; i < day; i++) {
+      gildedRose.updateQuality();
+    }
+    expect(gildedRose.items[0].quality).to.equal(2);
+    expect(gildedRose.items[0].sellIn).to.equal(-1);
+  });
+
+  it("should quality not be negative", () => {
+    const day = 7;
+    const gildedRose = new GildedRose([
+      new Item(spacialItemName.conjured, 5, 10),
+    ]);
+    for (let i = 0; i < day; i++) {
+      gildedRose.updateQuality();
+    }
+    expect(gildedRose.items[0].quality).to.equal(0);
+    expect(gildedRose.items[0].sellIn).to.equal(-2);
   });
 });
